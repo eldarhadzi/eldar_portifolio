3085ece73093a33f2c571b31d8fb1f8f491b93bd
Eldar Hadžović <128143237+eldarhadzi@users.noreply.github.com>
2025-09-24 10:42:00 +0200

    update first page

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ const Home = () => {
 
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Software Developer</span>
+            <span className="text-xl">Business Technologist</span>
             <h1 className="h1 mb-6">
               Hello I'm <br /> <span className="text-accent">Eldar Hadžović</span>
             </h1>
