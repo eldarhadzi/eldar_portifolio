b3acc760986016c4d51916437c0041335bdc39a3
eldarhadzi <eldarhadzovic03@gmail.com>
2026-09-19 19:14:02 +0200

    feat: switch home photo back to PNG

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -21,7 +21,7 @@ const Photo = () => {
         }}
          className="w-[288px] h-[288px] xl:w-[488px] xl:h-[488px] rounded-full overflow-hidden absolute mix-blend-lighten">
           <Image 
-            src="/assets/eldarone.jpeg" 
+            src="/assets/eldarone.png" 
             priority 
             quality={100} 
             fill 
